06741cf3c578c7693662893f519f04931381963e
Nicolas Brousse <pro@nicolas-brousse.fr>
2014-12-09 02:53:10 +0100

    Update ProductImage model (#

---
+++ src/Subbly/Model/Observer/ProductImageObserver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Subbly\Model\Observer;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+use Subbly\Model\ProductImage;
+use Subbly\Subbly;
+
+class ProductImageObserver
+{
+    /**
+     * 
+     */
+    public function created(ProductImage $model)
+    {
+        $this->uploadImage($model);
+    }
+
+    /**
+     * 
+     */
+    public function updated(ProductImage $model)
+    {
+        $this->uploadImage($model);
+    }
+
+    /**
+     * 
+     */
+    private function uploadImage(ProductImage $model)
+    {
+        if (!$model->imageToUpload instanceof UploadedFile) {
+            return;
+        }
+
+        $source = Subbly::getContainer()->get('media_resolver')->getSource($model, array(
+            'field' => 'filename',
+        ));
+
+        $model->imageToUpload->move(dirname($source), $model->filename);
+    }
+}
--- src/Subbly/Model/ProductImage.php
+++ src/Subbly/Model/ProductImage.php
@@ -3,6 +3,13 @@
 namespace Subbly\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+
+use Subbly\Subbly;
 
 class ProductImage extends Model implements ModelInterface
 {
@@ -22,6 +29,8 @@ class ProductImage extends Model implements ModelInterface
 
     protected $fillable = array('filename', 'image');
 
+    public $imageToUpload = null;
+
     public $sortable = array(
         'order_column_name' => 'position',
     );
@@ -36,10 +45,51 @@ class ProductImage extends Model implements ModelInterface
     );
 
     /**
+     *
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        ProductImage::observe(new Observer\ProductImageObserver);
+    }
+
+    /**
+     *
+     */
+    protected function performInsert(\Illuminate\Database\Eloquent\Builder $query, array $options)
+    {
+        $this->attributes['uid'] = md5(uniqid(mt_rand(), true));
+
+        parent::performInsert($query, $options);
+    }
+
+    /**
      * Relashionship
      */
     public function product()
     {
         return $this->belongsTo('Subbly\\Model\\Product', 'product_id');
+    }
+
+    /**
+     *
+     */
+    public function setImageAttribute($file)
+    {
+        if (!$file instanceof UploadedFile) {
+            return;
+        }
+
+        $this->imageToUpload = $file;
+
+        $this->attributes['filename'] = sprintf('%s.%s',
+            hash('sha256', uniqid(mt_rand(), true)),
+            $file->getClientOriginalExtension()
+        );
+    }
+    public function getImageAttribute()
+    {
+        return $this->attributes['filename'];
     }
 }
