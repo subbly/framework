ef2973f35b6034b1c4bff518588c4c038b4e446b
Nicolas Brousse <pro@nicolas-brousse.fr>
2015-04-16 22:54:15 +0200

    Update test autoload file

--- tests/autoload.php
+++ tests/autoload.php
@@ -2,6 +2,13 @@
 
 define('SUBBLY_TEST_ENV', TRUE);
 
+mkdir(__DIR__.'/dummy');
+mkdir(__DIR__.'/dummy/themes');
+mkdir(__DIR__.'/dummy/storage');
+mkdir(__DIR__.'/dummy/storage/cache');
+mkdir(__DIR__.'/dummy/storage/meta');
+mkdir(__DIR__.'/dummy/storage/sessions');
+
 $testEnvironment = 'testing';
 
 require_once __DIR__ . '/../vendor/autoload.php';
